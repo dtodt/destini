e9b6c6ef1e479862d476768493f491c80aceebb0
dtodt <danieltodt@gmail.com>
2020-01-19 21:24:42 -0300

    feat: story title and choices

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:destini/story_brain.dart';
 import 'package:flutter/material.dart';
 
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
@@ -15,7 +16,7 @@ class DestiniApp extends StatelessWidget {
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+final StoryBrain _storyBrain = new StoryBrain();
 
 class StoryPage extends StatefulWidget {
   StoryPage({Key key}) : super(key: key);
@@ -45,8 +46,7 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
+                    _storyBrain.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
@@ -62,8 +62,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.red,
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    _storyBrain.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -84,8 +83,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.blue,
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 1.
-                    'Choice 2',
+                    _storyBrain.getChoice2(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
